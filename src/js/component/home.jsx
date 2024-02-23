8ddbef6df07cd92b82005a8b38e18fc2fcd5c399
Natalia <nlopezsalas@gmail.com>
2024-02-23 21:25:07 +0000

    Create first step tic tac toe

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// Create your first component
 const Home = () => {
+	const [ticTacToe, setTicTacToe] = useState([
+		[undefined, undefined, undefined],
+		[undefined, undefined, undefined],
+		[undefined, undefined, undefined]
+	]);
+
+	const [turn, setTurn] = useState("X");
+
+	const changeTurn = () => setTurn(turn === "X" ? "O" : "X"); 
+
+	const setValue = ( rowIndex, colIndex) => {
+		if (typeof ticTacToe[rowIndex][colIndex] === "undefined") {
+			let aux = ticTacToe;
+			aux[rowIndex][colIndex] = turn;
+			setTicTacToe(aux);
+			changeTurn();
+		}
+		else {
+			alert("This place is taken... sorry!");
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center d-flex flex-column w-50 mx-auto m-5">
+			<h1 className="mb-4">It's {turn} turn</h1>
+			<table>
+				<tbody>
+					{ticTacToe.map((row, rowIndex) => (
+						<tr key={rowIndex}>
+							{row.map((col, colIndex) => (
+								<td key={colIndex} onClick={() => setValue(rowIndex, colIndex)}>
+									<div className="cell">{col}</div>
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 };
